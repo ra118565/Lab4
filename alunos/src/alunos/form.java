9811a2482e89873f18dd80cda96d76d4995fbc7f
118565 <ricardo.peloi@gmail.com>
2015-04-14 00:42:18 -0300

    Finalização do design do forms

--- alunos/src/alunos/form.java
+++ alunos/src/alunos/form.java
@@ -21,7 +21,13 @@ public class form extends JFrame {
 	public JTextField textfirstName;
 	public JTextField textlastName;
 	public JTextField textNascimentoDia,textNascimentoMes, textNascimentoAno;
-
+	public JTextField textCPF;
+	public JTextField textDDI, textDDD, textNum;
+	public JTextField textAdr1;
+	public JTextField textCompl;
+	public JTextField textCEP, textCEP2;
+	public JTextField textCity, textState, textCountry;
+	
 	
 	/**
 	 * Launch the application.
@@ -44,34 +50,16 @@ public class form extends JFrame {
 	 */
 	public form() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 245);
+		setBounds(100, 100, 470, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textfirstName = new JTextField();
-		textfirstName.setBounds(99, 58, 114, 19);
-		contentPane.add(textfirstName);
-		textfirstName.setColumns(10);
-		
-		textlastName = new JTextField();
-		textlastName.setBounds(99, 89, 114, 19);
-		contentPane.add(textlastName);
-		textlastName.setColumns(10);
-		
-		textNascimentoDia = new JTextField();
-		textNascimentoDia.setBounds(99, 120, 25, 19);
-		contentPane.add(textNascimentoDia);
-		textNascimentoDia.setColumns(10);
-		textNascimentoMes = new JTextField();
-		textNascimentoMes.setBounds(132, 120, 25, 19);
-		contentPane.add(textNascimentoMes);
-		textNascimentoMes.setColumns(10);
-		textNascimentoAno = new JTextField();
-		textNascimentoAno.setBounds(164, 120, 50, 19);
-		contentPane.add(textNascimentoAno);
-		textNascimentoAno.setColumns(10);
+//DROP DOWN MENU
+		JLabel lblTitle = new JLabel("Title");
+		lblTitle.setBounds(12, 17, 90, 15);
+		contentPane.add(lblTitle);
 		
 		final JComboBox comboTitle = new JComboBox();
 		comboTitle.setModel(new DefaultComboBoxModel(new String[] {"Mr.", "Mrs.", "Miss.", "Ms.", "Dr.", "Other"}));
@@ -79,18 +67,173 @@ public class form extends JFrame {
 		comboTitle.setBounds(58, 12, 81, 24);
 		contentPane.add(comboTitle);
 		
-		JLabel lblFirstName = new JLabel("First Name:");
+//PRIMEIRO NOME
+		JLabel lblFirstName = new JLabel("First Name*");
 		lblFirstName.setBounds(12, 60, 90, 15);
 		contentPane.add(lblFirstName);
 		
-		JLabel lblLastName = new JLabel("Last Name:");
-		lblLastName.setBounds(12, 91, 90, 15);
+		textfirstName = new JTextField();
+		textfirstName.setBounds(12, 80, 114, 19);
+		contentPane.add(textfirstName);
+		textfirstName.setColumns(10);
+		
+//ULTIMO NOME
+		JLabel lblLastName = new JLabel("Last Name*");
+		lblLastName.setBounds(150, 60, 90, 15);
 		contentPane.add(lblLastName);
 		
-		JLabel lblTitle = new JLabel("Title:");
-		lblTitle.setBounds(12, 17, 90, 15);
-		contentPane.add(lblTitle);
+		textlastName = new JTextField();
+		textlastName.setBounds(150, 80, 114, 19);
+		contentPane.add(textlastName);
+		textlastName.setColumns(10);
 		
+//DIA MES E ANO DE NASCIMENTO
+		JLabel lblDataNiver = new JLabel("Data de nascimento*");
+		lblDataNiver.setBounds(290, 60, 150, 15);
+		contentPane.add(lblDataNiver);
+
+		JLabel lblDiaNiver = new JLabel("Dia");
+		lblDiaNiver.setBounds(320, 83, 30, 15);
+		contentPane.add(lblDiaNiver);
+		textNascimentoDia = new JTextField();
+		textNascimentoDia.setBounds(355, 80, 25, 19);
+		contentPane.add(textNascimentoDia);
+		textNascimentoDia.setColumns(10);
+		
+		JLabel lblMesNiver = new JLabel("Mês");
+		lblMesNiver.setBounds(320, 105, 30, 15);
+		contentPane.add(lblMesNiver);
+		textNascimentoMes = new JTextField();
+		textNascimentoMes.setBounds(355, 102, 25, 19);
+		contentPane.add(textNascimentoMes);
+		textNascimentoMes.setColumns(10);
+		
+		JLabel lblAnoNiver = new JLabel("Ano");
+		lblAnoNiver.setBounds(320, 127, 30, 15);
+		contentPane.add(lblAnoNiver);		
+		textNascimentoAno = new JTextField();
+		textNascimentoAno.setBounds(355, 124, 50, 19);
+		contentPane.add(textNascimentoAno);
+		textNascimentoAno.setColumns(10);
+		
+//EMAIL
+		JLabel lblEmail = new JLabel("E-mail*");
+		lblEmail.setBounds(12, 127, 90, 15);
+		contentPane.add(lblEmail);
+		
+		textlastName = new JTextField();
+		textlastName.setBounds(70, 124, 195, 19);
+		contentPane.add(textlastName);
+		textlastName.setColumns(10);		
+		
+//CPF
+		JLabel lblCPF = new JLabel("CPF*");
+		lblCPF.setBounds(12, 164, 90, 15);
+		contentPane.add(lblCPF);
+		
+		textCPF = new JTextField();
+		textCPF.setBounds(70, 161, 130, 19);
+		contentPane.add(textCPF);
+		textCPF.setColumns(10);
+		
+//Phone
+		JLabel lblPhone = new JLabel("Phone*");
+		lblPhone.setBounds(240, 185, 90, 15);
+		contentPane.add(lblPhone);
+		
+		JLabel lblPhoneDDD = new JLabel("DDI       +");
+		lblPhoneDDD.setBounds(310, 163, 90, 15);
+		contentPane.add(lblPhoneDDD);
+		textDDI = new JTextField();
+		textDDI.setBounds(376, 161, 30, 19);
+		contentPane.add(textDDI);
+		textDDI.setColumns(10);	
+		
+		JLabel lblPhoneDDI = new JLabel("DDD");
+		lblPhoneDDI.setBounds(310, 185, 90, 15);
+		contentPane.add(lblPhoneDDI);
+		textDDD = new JTextField();
+		textDDD.setBounds(376, 183, 30, 19);
+		contentPane.add(textDDD);
+		textDDD.setColumns(10);	
+		
+		
+		JLabel lblPhoneNumero = new JLabel("Número");
+		lblPhoneNumero.setBounds(310, 207, 90, 15);
+		contentPane.add(lblPhoneNumero);
+		textNum = new JTextField();
+		textNum.setBounds(376, 205, 80, 19);
+		contentPane.add(textNum);
+		textNum.setColumns(10);	
+		
+//Adress
+		JLabel lblAdress1 = new JLabel("Adress");
+		lblAdress1.setBounds(12, 230, 90, 15);
+		contentPane.add(lblAdress1);
+
+		textAdr1 = new JTextField();
+		textAdr1.setBounds(12, 250, 418, 19);
+		contentPane.add(textAdr1);
+		textAdr1.setColumns(10);
+		
+		JLabel lblCompl = new JLabel("Apt, floor, suite, etc");
+		lblCompl.setBounds(12, 282, 240, 15);
+		contentPane.add(lblCompl);
+		
+		textCompl = new JTextField();
+		textCompl.setBounds(170, 280, 260, 19);
+		contentPane.add(textCompl);
+		textCompl.setColumns(10);
+			
+		
+//Adress
+		JLabel lblCEP = new JLabel("CEP");
+		lblCEP.setBounds(12, 322, 90, 15);
+		contentPane.add(lblCEP);
+
+		textCEP = new JTextField();
+		textCEP.setBounds(50, 320, 50, 19);
+		contentPane.add(textCEP);
+		textCEP.setColumns(10);
+		
+		JLabel lblCEP2 = new JLabel("-");
+		lblCEP2.setBounds(103, 320, 10, 15);
+		contentPane.add(lblCEP2);
+		
+		textCEP2 = new JTextField();
+		textCEP2.setBounds(113, 320, 30, 19);
+		contentPane.add(textCEP2);
+		textCEP2.setColumns(10);
+		
+//CIDADE, ESTADO, PAIS		
+		JLabel lblCity = new JLabel("City");
+		lblCity.setBounds(12, 352, 100, 15);
+		contentPane.add(lblCity);
+		
+		textCity = new JTextField();
+		textCity.setBounds(50, 350, 150, 19);
+		contentPane.add(textCity);
+		textCity.setColumns(10);
+		
+		JLabel lblState = new JLabel("State");
+		lblState.setBounds(220, 322, 100, 15);
+		contentPane.add(lblState);
+		
+		textState = new JTextField();
+		textState.setBounds(280, 320, 150, 19);
+		contentPane.add(textState);
+		textState.setColumns(10);
+		
+		JLabel lblCountry = new JLabel("Country");
+		lblCountry.setBounds(220, 352, 100, 15);
+		contentPane.add(lblCountry);
+		
+		textCountry = new JTextField();
+		textCountry.setBounds(280, 350, 150, 19);
+		contentPane.add(textCountry);
+		textCountry.setColumns(10);
+		
+//BOTÃO DE PRINT
 		JButton btnPrint = new JButton("Print");
 		btnPrint.addActionListener(new ActionListener() {
 			@SuppressWarnings("deprecation")
@@ -104,7 +247,7 @@ public class form extends JFrame {
 				printForm.show();	
 			}
 		});
-		btnPrint.setBounds(12, 149, 74, 25);
+		btnPrint.setBounds(180, 400, 74, 25);
 		contentPane.add(btnPrint);
 	}
 }
